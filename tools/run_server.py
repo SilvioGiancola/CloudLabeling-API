d4b3d1b916c15da105e246c63d23c62f47e30354
Silvio Giancola <silvio.giancola@gmail.com>
2021-05-28 04:07:40 +0300

    server add choice GPU

--- tools/run_server.py
+++ tools/run_server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, send_from_directory       
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from cloudlabeling import server as cloudserver
-
+import os
 
 app = Flask(__name__)
 import boto3
@@ -33,8 +33,14 @@ if __name__ == "__main__":
                         
     parser.add_argument("--PORT", type=int, default=4000,
                         help="commmunication port")
+    parser.add_argument('--GPU',        required=False, type=int,
+                        default=-1,     help='ID of the GPU to use')
 
     args = parser.parse_args()
+
+    if args.GPU >= 0:
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(args.GPU)
 
     server = cloudserver.FlaskServer(args.HOST, args.PORT)
     app.run(host=server.HOST, port=server.PORT)
