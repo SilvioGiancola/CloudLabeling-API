5a47b52a5c62250f6ac971514016cb90f382473a
SilvioGiancola <silvio.giancola@gmail.com>
2022-03-29 15:35:25 +0300

    minor commit for v0.0.16

--- cloudlabeling/__init__.py
+++ cloudlabeling/__init__.py
@@ -1,5 +1,5 @@
 
-__version_info__ = ('0', '0', '15')
+__version_info__ = ('0', '0', '16')
 __version__ = '.'.join(__version_info__)
 __authors__ = "Silvio Giancola"
 __authors_username__ = "giancos"
